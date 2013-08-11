4e7af6c188675c72410eaed0c1ad14ecbfe341b5
Thomas Kölling <spamdoggi@gmail.com>
2013-08-11 20:34:55 +0200

    switched to relative path for better customizability

--- owncloud_backup.sh
+++ owncloud_backup.sh
@@ -1,17 +1,18 @@
-#!/bin/bash          
-SRCD1="/var/www/owncloud/data"
-SRCD2="/var/www/owncloud/config"
+#!/bin/bash
+OWNCLOUDROOT="/var/www/owncloud/"
+SRCD1="data"
+SRCD2="config"
 TGTD="/vagrant/backups/"
 OF=owncloud-backup-$(date +%Y%m%d).tar
 
-DBDUMPD="/vagrant/backups/db"
+DBDUMPD="db"
 # read actual db passwd from config for sql dump
 DBPASSWD=`sudo php -r 'include("/var/www/owncloud/config/config.php"); echo $CONFIG["dbpassword"];'`
 
 mkdir -p $TGTD
 
 echo backup ownclouds data and config folder
-tar -Pcf $TGTD$OF $SRCD1 $SRCD2
+cd $OWNCLOUDROOT && tar -cf $TGTD$OF $SRCD1 $SRCD2
 
 mkdir -p $DBDUMPD
 
@@ -19,7 +20,7 @@ echo backup mysql database
 mysqldump -u owncloud -p$DBPASSWD -l > $DBDUMPD/owncloud_db.sql
 
 echo adding dbdump to tar
-tar -Prf $TGTD$OF $DBDUMPD/owncloud_db.sql
+tar -rf $TGTD$OF $DBDUMPD/owncloud_db.sql
 
 echo compressing backup
 gzip $TGTD$OF
